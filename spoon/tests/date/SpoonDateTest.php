6c3ee02f29db5431854276de493d69a7ab93a130
Bart Lagerweij <bart@webleads.nl>
2015-03-30 14:12:44 +0200

    getTimeAgo: Loading locale data only once and keep it in a class field. - preventing "Notice: Undefined index: time..." errors - Added tests

--- spoon/tests/date/SpoonDateTest.php
+++ spoon/tests/date/SpoonDateTest.php
@@ -14,4 +14,16 @@ class SpoonDateTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(date('Y-m-d H:i'), SpoonDate::getDate('Y-m-d H:i'));
 		$this->assertEquals(SpoonDate::getDate('l j F Y', mktime(13, 20, 0, 8, 3, 1983)), 'Wednesday 3 August 1983');
 	}
+
+	public function testGetTimeAgo()
+	{
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-60), '1 minute ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(2*60)), '2 minutes ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(60*60)), '1 hour ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(2*60*60)), '2 hours ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(24*60*60)), '1 day ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(2*24*60*60)), '2 days ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(7*24*60*60)), '1 week ago');
+		$this->assertEquals(SpoonDate::getTimeAgo(time()-(14*24*60*60)), '2 weeks ago');
+	}
 }
